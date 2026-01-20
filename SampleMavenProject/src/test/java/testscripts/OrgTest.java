0bcad7b47c4b831418a183954c5a6e377d03e10b
Gvinu <Gvinu@Vinusha_Mahesh.mshome.net>
2026-01-20 11:09:52 +0530

    added sample project to gitHub

---
+++ SampleMavenProject/src/test/java/testscripts/OrgTest.java
@@ -0,0 +1,27 @@
+package testscripts;
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void testTest()
+	{
+		System.out.println("sample org1");
+		System.out.println("sample org2.");
+		System.out.println("sample org3");
+		System.out.println("sample org4");
+		System.out.println("sample org5");
+		
+	}
+	
+	@Test
+	public void orgTest()
+	{
+		System.out.println("sample org1");
+		System.out.println("sample org2.");
+		System.out.println("sample org3");
+		System.out.println("sample org4");
+		System.out.println("sample org5");
+		
+	}
+
+}
---
+++ SampleMavenProject/src/test/java/testscripts/SampleTest.java
@@ -0,0 +1,28 @@
+package testscripts;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void testTest()
+	{
+		
+		System.out.println(System.getProperty("browser"));
+		System.out.println("sample test2.");
+		System.out.println("sample test3");
+		System.out.println("sample test4");
+		System.out.println("sample test5");
+		
+	}
+	
+	@Test
+	public void sampleTest()
+	{
+		System.out.println(System.getProperty("browser"));
+		System.out.println("sample test2.");
+		System.out.println("sample test3");
+		System.out.println("sample test4");
+		System.out.println("sample test5");
+		
+	}
+
+}
